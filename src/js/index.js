55f790ae9033a4829ec6ef2feb90fa002951fc79
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-09 17:04:44 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,13 +7,6 @@ import { animate } from "./animation.js";
 
 /******************************************************* SETUP ************************************************************/
 
-if (/Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent)) {
-    // This code will run only on Android smartphones
-} else {
-    document.getElementsByTagName('html')[0].innerHTML = '<h1>Hey babe, this site works on Android smartphones only!</h1>';
-}
-
-
 if (process.env.OPEN_DATE) {
   const status = isBDay();
   if (status === "IS_EARLY") setPage(soon);
