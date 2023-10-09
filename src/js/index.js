e1f88bc8278da3add0cac59cfb1d66935215239c
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-09 16:28:42 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,43 @@ if (/Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent))
     document.getElementsByTagName('html')[0].innerHTML = '<h1>Hey babe, this site works on Android smartphones only!</h1>';
 }
 
+
+let isFrozen = false;
+
+function freezeWebsite() {
+    if (!isFrozen) {
+        // Freeze the website
+        document.body.style.pointerEvents = 'none';
+        isFrozen = true;
+    }
+}
+
+function unfreezeWebsite() {
+    if (isFrozen) {
+        // Unfreeze the website
+        document.body.style.pointerEvents = 'auto';
+        isFrozen = false;
+    }
+}
+
+window.addEventListener('resize', function () {
+    if (window.orientation === 0 || window.orientation === 180) {
+        // Portrait orientation
+        unfreezeWebsite();
+    } else {
+        // Landscape orientation
+        freezeWebsite();
+    }
+});
+
+// Check the orientation on page load
+window.addEventListener('load', function () {
+    if (window.orientation !== undefined && (window.orientation === 90 || window.orientation === -90)) {
+        // Landscape orientation on page load
+        freezeWebsite();
+    }
+});
+
 if (process.env.OPEN_DATE) {
   const status = isBDay();
   if (status === "IS_EARLY") setPage(soon);
