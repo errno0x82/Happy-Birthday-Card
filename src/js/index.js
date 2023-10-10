88acec6a6c95947618d751526b4a905e034778f1
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-10 08:58:28 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -12,27 +12,6 @@ if (/Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent))
     document.getElementsByTagName('html')[0].innerHTML = '<h1>Hey babe, this site works on Android smartphones only!</h1>';
 }
 
-        
-var isFrozen = false; // Flag to track if content is frozen
-
-window.addEventListener("orientationchange", function () {
-    var body = document.body;
-
-    if (window.orientation === 0) {
-        // Portrait mode
-        if (isFrozen) {
-            body.style.display = "block"; // Resume HTML code execution
-            isFrozen = false; // Reset the flag
-        }
-    } else {
-        // Landscape mode
-        body.style.display = "none"; // Halt HTML code execution
-        isFrozen = true; // Set the flag to indicate content is frozen
-    }
-});
-
-
-
 if (process.env.OPEN_DATE) {
   const status = isBDay();
   if (status === "IS_EARLY") setPage(soon);
