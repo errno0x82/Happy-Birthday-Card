8ce53e5fb9bed825629886b227590f7569f92bde
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-10 08:35:35 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -12,22 +12,18 @@ if (/Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent))
     document.getElementsByTagName('html')[0].innerHTML = '<h1>Hey babe, this site works on Android smartphones only!</h1>';
 }
 
-window.addEventListener('resize', function () {
-    if (window.orientation === 0 || window.orientation === 180) {
-        document.querySelector('.landscape-message').style.display = 'none';
-        document.body.style.overflow = 'auto';
-    } else {
-        document.querySelector('.landscape-message').style.display = 'block';
-        document.body.style.overflow = 'hidden';
-    }
-});
+        
+window.addEventListener("orientationchange", function () {
+            var contentDiv = document.getElementById("content");
 
-window.addEventListener('load', function () {
-    if (window.orientation !== undefined && (window.orientation === 90 || window.orientation === -90)) {
-        document.querySelector('.landscape-message').style.display = 'block';
-        document.body.style.overflow = 'hidden';
-    }
-});
+            if (window.orientation === 0) {
+                // Portrait mode
+                contentDiv.style.display = "block";
+            } else {
+                // Landscape mode
+                contentDiv.style.display = "none";
+            }
+});    
 
 if (process.env.OPEN_DATE) {
   const status = isBDay();
