4514e973e6f55a5edc82d8aec2bc88de4225d33c
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-11 20:02:25 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -12,9 +12,23 @@ if (/Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent))
     document.getElementsByTagName('html')[0].innerHTML = '<h1>Hey babe, this site works on Android smartphones only!</h1>';
 }
 
-// Display an alert when the page loads
-window.addEventListener("load", function() {
-    alert("Please increase the volume to the maximum.");
+document.addEventListener("DOMContentLoaded", function () {
+  var volumeMessage = document.createElement("divvbvvv");
+  volumeMessage.innerText = "Consider increasing your volume for a better experience!";
+  volumeMessage.style.position = "fixed";
+  volumeMessage.style.bottom = "20px";
+  volumeMessage.style.left = "20px";
+  volumeMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  volumeMessage.style.color = "#fff";
+  volumeMessage.style.padding = "10px";
+  volumeMessage.style.borderRadius = "5px";
+  volumeMessage.style.zIndex = "9999";
+  document.body.appendChild(volumeMessage);
+
+  // Close the message after a few seconds (e.g., 5 seconds)
+  setTimeout(function () {
+    volumeMessage.style.display = "none";
+  }, 5000);
 });
 
 if (process.env.OPEN_DATE) {
