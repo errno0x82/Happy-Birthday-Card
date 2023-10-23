615e3af34f90f1495fe36a25a5d453da0894ef97
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-23 12:11:00 +0530

    Update animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -27,7 +27,7 @@ function closePageWithCountdown(seconds) {
         loveText.style.position = "absolute";
         loveText.style.color = "red";
         loveText.style.fontWeight = "bold";
-        loveText.style.top = Math.floor(Math.random() * (window.innerHeight - 300)) + "px"; // Adjust the range as needed
+        loveText.style.top = Math.floor(Math.random() * (window.innerHeight + 100)) + "px"; // Adjust the range as needed
         var maxWidth = window.innerWidth - 300; // Adjust the width of the text
         loveText.style.left = Math.floor(Math.random() * maxWidth) + "px"; // Adjust the width as needed
         document.body.appendChild(loveText);
