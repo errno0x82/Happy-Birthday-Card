d76f7bd05fbab66753554a7736203e3566245e29
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-10-23 13:37:07 +0530

    Update animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -27,13 +27,9 @@ function closePageWithCountdown(seconds) {
         loveText.style.position = "absolute";
         loveText.style.color = "red";
         loveText.style.fontWeight = "bold";
-        // Calculate the maximum top position to ensure it stays above "Resource Exhausted" line
-        var maxTop = window.innerHeight - 200; // Adjust the value as needed
-        loveText.style.top = Math.floor(Math.random() * maxTop) + "px";
-    
+        
         var maxWidth = window.innerWidth - 300; // Adjust the width of the text
         loveText.style.left = Math.floor(Math.random() * maxWidth) + "px";
-        loveText.style.zIndex = "1000";
         document.body.appendChild(loveText);
         
         setTimeout(function () {
