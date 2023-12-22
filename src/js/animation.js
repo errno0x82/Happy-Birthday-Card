142186be0ba8e363593ba4196a37314afdb07ee7
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-12-23 00:03:03 +0530

    Update animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -47,7 +47,7 @@ function closePageWithCountdown(seconds) {
       
       // Create a new div element to hold the text
       var flashbackText = document.createElement('div');
-      flashbackText.innerHTML = 'Flashback: How it all started (Funny)';
+      flashbackText.innerHTML = 'Flashback: Old Memories (Funny animation)';
       flashbackText.style.position = 'absolute';
       flashbackText.style.top = '50%';
       flashbackText.style.left = '50%';
