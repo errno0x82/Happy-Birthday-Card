676cc51bc965ac57356ffe4f67a826e4c3f35b67
errno0x82 <144832717+errno0x82@users.noreply.github.com>
2023-12-25 22:21:14 +0530

    Update animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -76,7 +76,7 @@ function closePageWithCountdown(seconds) {
         video.controls = false;
 
         var source = document.createElement('source');
-        source.src = './Sriparna.mp4';
+        source.src = './src/js/Sriparna.mp4';
         source.type = 'video/mp4';
 
         video.appendChild(source);
